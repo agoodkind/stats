e6065bf051cc0a1d8ba7a4e2ad6c5af0ae8002a3
tarickb <tarick@bedeir.com>
2025-02-18 21:28:22 -0800

    Log contrib repo, query path.
    Signed-off-by: tarickb <tarick@bedeir.com>

--- github_stats.py
+++ github_stats.py
@@ -73,6 +73,8 @@ class Queries(object):
         :return: deserialized REST JSON output
         """
 
+        print("querying %s" % path)
+        
         for _ in range(60):
             headers = {
                 "Authorization": f"token {self.access_token}",
@@ -335,6 +337,7 @@ Languages:
 
             repos = owned_repos.get("nodes", [])
             if not self._ignore_forked_repos:
+                print("considering repo [%s]" % contrib_repos.get("nodes", []).get("nameWithOwner"))
                 repos += contrib_repos.get("nodes", [])
 
             for repo in repos:
